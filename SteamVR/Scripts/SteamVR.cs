3897b4c70886ed3b807178815bd829e21a3140f4
dooly <doolanl208@gmail.com>
2025-01-16 09:34:09 +1000

    overrided

--- SteamVR/Scripts/SteamVR.cs
+++ SteamVR/Scripts/SteamVR.cs
@@ -5,17 +5,9 @@
 //=============================================================================
 
 using UnityEngine;
-using Valve.VR;
 using System.IO;
-using System.Linq;
 
-#if UNITY_2017_2_OR_NEWER
 using UnityEngine.XR;
-#else
-using XRSettings = UnityEngine.VR.VRSettings;
-using XRDevice = UnityEngine.VR.VRDevice;
-#endif
-
 namespace Valve.VR
 {
     public class SteamVR : System.IDisposable
@@ -34,7 +26,6 @@ namespace Valve.VR
             {
                 if (_enabled)
                 {
-#if UNITY_2020_1_OR_NEWER || OPENVR_XR_API
                     if (isSupported == null)
                     {
                         string[] supportedDevices = XRSettings.supportedDevices;
@@ -51,12 +42,9 @@ namespace Valve.VR
                         }
                     }
                     return isSupported.Value;
-#else
-                    return XRSettings.enabled;
-#endif
                 }
 
-                return false;
+                return XRSettings.enabled;
             }
             set
             {
@@ -64,7 +52,7 @@ namespace Valve.VR
 
                 if (_enabled)
                 {
-                    Initialize();
+                    Debug.LogError("Missing the enable method use the initalization instead");
                 }
                 else
                 {
@@ -108,11 +96,11 @@ namespace Valve.VR
 
         public static InitializedStates initializedState = InitializedStates.None;
 
-        public static void Initialize(bool forceUnityVRMode = false)
+        public static void Initialize(SteamVR_Render renderInstance, SteamVR_Behaviour behaviourInstance, bool forceUnityVRMode = false)
         {
             if (forceUnityVRMode)
             {
-                SteamVR_Behaviour.instance.InitializeSteamVR(true);
+                SteamVR_Behaviour.Initialize(renderInstance, behaviourInstance, forceUnityVRMode);
                 return;
             }
             else
@@ -126,7 +114,7 @@ namespace Valve.VR
             }
 
             if (_enabled)
-                SteamVR_Behaviour.Initialize(forceUnityVRMode);
+                SteamVR_Behaviour.Initialize(renderInstance, behaviourInstance, forceUnityVRMode);
         }
 
         public static bool usingNativeSupport
@@ -139,28 +127,7 @@ namespace Valve.VR
         private static void ReportGeneralErrors()
         {
             string errorLog = "<b>[SteamVR]</b> Initialization failed. ";
-
-#if OPENVR_XR_API
             errorLog += "Please verify that you have SteamVR installed, your hmd is functioning, and OpenVR Loader is checked in the XR Plugin Management section of Project Settings.";
-#else
-
-            if (XRSettings.enabled == false)
-                errorLog += "VR may be disabled in player settings. Go to player settings in the editor and check the 'Virtual Reality Supported' checkbox'. ";
-            if (XRSettings.supportedDevices != null && XRSettings.supportedDevices.Length > 0)
-            {
-                if (XRSettings.supportedDevices.Contains("OpenVR") == false)
-                    errorLog += "OpenVR is not in your list of supported virtual reality SDKs. Add it to the list in player settings. ";
-                else if (XRSettings.supportedDevices.First().Contains("OpenVR") == false)
-                    errorLog += "OpenVR is not first in your list of supported virtual reality SDKs. <b>This is okay, but if you have an Oculus device plugged in, and Oculus above OpenVR in this list, it will try and use the Oculus SDK instead of OpenVR.</b> ";
-            }
-            else
-            {
-                errorLog += "You have no SDKs in your Player Settings list of supported virtual reality SDKs. Add OpenVR to it. ";
-            }
-
-            errorLog += "To attempt to force OpenVR initialization call SteamVR.Initialize(true). ";
-#endif
-
             Debug.LogWarning(errorLog);
         }
 
@@ -171,16 +138,6 @@ namespace Valve.VR
             try
             {
                 var error = EVRInitError.None;
-
-#if !OPENVR_XR_API
-                if (!SteamVR.usingNativeSupport)
-                {
-                    ReportGeneralErrors();
-                    initializedState = InitializedStates.InitializeFailure;
-                    SteamVR_Events.Initialized.Send(false);
-                    return null;
-                }
-#endif
 
                 // Verify common interfaces are valid.
 
@@ -213,23 +170,12 @@ namespace Valve.VR
                 }
 
                 settings = SteamVR_Settings.instance;
-
-#if !OPENVR_XR_API
-                if (Application.isEditor)
-                    IdentifyEditorApplication();
-
-                SteamVR_Input.IdentifyActionsFile();
-#endif
-
-                if (SteamVR_Settings.instance.inputUpdateMode != SteamVR_UpdateModes.Nothing || SteamVR_Settings.instance.poseUpdateMode != SteamVR_UpdateModes.Nothing)
-                {
-                    SteamVR_Input.Initialize();
+         //       SteamVR_Input.Initialize();
 
 #if UNITY_EDITOR
-                    if (SteamVR_Input.IsOpeningSetup())
-                        return null;
+                if (SteamVR_Input.IsOpeningSetup())
+                    return null;
 #endif
-                }
             }
             catch (System.Exception e)
             {
